a3561be6b4d76ad56887016270497ce7d3bf0a71
MinosDev84 <148528531+MinosDev84@users.noreply.github.com>
2023-10-25 02:06:29 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.29 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string solution(string phone_number) {
+        string answer = "";
+        
+        for(int i = 0; i < phone_number.Length; ++i)
+            answer += (i >= phone_number.Length - 4) ? phone_number[i] : '*';
+        
+        return answer;
+    }
+}
