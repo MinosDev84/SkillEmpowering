17702db46bc42261cf441b58891c635c8469b294
MinosDev84 <148528531+MinosDev84@users.noreply.github.com>
2023-10-25 17:01:54 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.32 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884#) 
 
 ### 성능 요약
 
-메모리: 31.2 MB, 시간: 0.20 ms
+메모리: 31.5 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 16:59:58
+2023년 10월 3일 17:1:53
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -4,13 +4,16 @@ public class Solution {
     public int solution(int left, int right) {
         int answer = 0;
         
-        int squared = 0;
-        for(int j = 0; j <= System.Math.Sqrt(right); ++j)
-                squared += ((j * j >= left) && (j * j <= right)) ? (j * j) : 0;
-        
+        // 일단 left ~ right 전부 더한다
         for(int i = left; i <= right; ++i)
             answer += i;
+        
+        // 약수가 홀수이려면 제곱수여야 하므로, left ~ right 사이의 제곱수를 전부 더해둔다.
+        int squared = 0;
+        for(int j = 1; j <= System.Math.Sqrt(right); ++j)
+                squared += ((j * j >= left) && (j * j <= right)) ? (j * j) : 0;   
                 
+        // left ~ right 전부 더한 값에서 제곱수들의 합을 2번 빼준다(이미 더해졌고 + 빼줘야하므로)
         return answer - (squared * 2);
     }
 }
