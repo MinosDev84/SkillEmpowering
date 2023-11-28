26f7f12f2e65d2f15fe6d8e0cca4ed04cdbc964e
MinosDev84 <148528531+MinosDev84@users.noreply.github.com>
2023-11-28 20:36:23 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int h, m;
+    
+    cin >> h >> m;
+    if(m < 45)
+    {
+        --h;
+        if(h < 0)
+            h += 24;
+        m += 15;
+    }
+    else
+        m -= 45;
+        
+    cout << h << " " << m << endl;
+    
+    return 0;
+}
